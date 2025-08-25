9fc165ea578c39d38cc2a0153bb854f15a196571
OsamaBaakeem <osamabaakeem.courses@gmail.com>
2025-08-25 20:25:46 +0300

    return uploaded file id

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -19,7 +19,7 @@ class DataController(BaseController):
 
         return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
 
-    def generate_unique_filename(self, orig_file_name: str, project_id: str):
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
 
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id=project_id)
@@ -40,7 +40,7 @@ class DataController(BaseController):
                 random_key + "_" + cleaned_file_name
             )
 
-        return new_file_path
+        return new_file_path, random_key + "_" + cleaned_file_name
 
     def get_clean_filename(self, orig_file_name: str):
 
